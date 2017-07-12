e575932ecfca36de5b31cef93824d581bb638b7d
Guest User <Guest@Epicodus-1C.local>
2017-07-12 11:28:40 -0700

    Add album display on index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
 
+  root to: 'albums#index'
 
+  resources :accounts do
+    resources :orders
+  end
+
+  resources :orders do
+    resources :order_items
+  end
+
+  resources :albums do
+    resources :songs
+  end
 end
