09659d14e50c13b8b441f1f1dbecfb43fd3f9114
Aubrey Masten and James Higgins <aubrey+james@>
2017-07-12 09:54:45 -0700

    add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  belongs_to :account, foreign_key: :account_id, optional: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
