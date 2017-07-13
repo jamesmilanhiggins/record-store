a263d67ecd182c93d25762c50bb7a317970ce695
Aubrey Masten and James Higgins <aubrey+james@>
2017-07-13 16:05:32 -0700

    allow unauthorized users to view album details

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,5 @@
 class AlbumsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @albums = Album.in_stock
