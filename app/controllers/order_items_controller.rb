798a469845113038a8fd12b0b4422ed9c7b77de3
Aubrey Masten and James Higgins <aubrey+james@>
2017-07-13 08:42:28 -0700

    add quantity updating to existing cart items

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,7 +1,16 @@
 class OrderItemsController < ApplicationController
   def create
     @order = Order.find(params[:order_id])
-    @order_item = @order.order_items.create(order_item_params)
+    @order_item = @order.order_items.find {|item| item.album_id == order_item_params[:album_id].to_i }
+    # if order_item of selected album_id already exists
+    # add requested quantity to that order_item
+    # otherwise, the album is not in the cart
+    # so add it to the cart, with the requested quantity
+    if @order_item
+      @order_item.update(quantity: @order_item.quantity + order_item_params[:quantity].to_i)
+    else
+      @order_item = @order.order_items.create(order_item_params)
+    end
     if @order_item.save
       # add dynamic alert message
       respond_to do |format|
