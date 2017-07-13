d131f2c81843c0b7d960cb83fa141310b071c975
Aubrey Masten and James Higgins <aubrey+james@>
2017-07-13 09:52:37 -0700

    add display for number of items in cart  and limit max order by stock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   helper_method :update_item_stock
   helper_method :cart_total_price
   helper_method :cart_count
+  helper_method :find_cart_item_by_id
+  helper_method :valid_cart_total?
+  helper_method :cart_quantity
 
   def cart
     current_user.account.orders.where("status = 'Active'").first if current_user
@@ -24,6 +27,31 @@ class ApplicationController < ActionController::Base
   def cart_count
     cart.order_items.sum do |item|
       item.quantity
+    end
+  end
+
+  def find_cart_item_by_id(cart_id, order_item_id)
+    cart = Order.find(cart_id)
+    cart.order_items.find {|item| item.album_id == order_item_id }
+  end
+
+  def valid_cart_total?(new_order)
+    item_in_cart = find_cart_item_by_id(cart.id, new_order[:album_id].to_i)
+    if item_in_cart
+      requested_stock = item_in_cart.quantity + new_order[:quantity].to_i
+    else
+      requested_stock = new_order[:quantity].to_i
+    end
+    album = Album.find(new_order[:album_id].to_i)
+    album.quantity >= requested_stock
+  end
+
+  def cart_quantity(cart, album_id)
+    item = cart.order_items.find{ |item| item.album_id == album_id }
+    if item
+      item.quantity
+    else
+      0
     end
   end
 end
--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,25 +1,27 @@
 class OrderItemsController < ApplicationController
   def create
     @order = Order.find(params[:order_id])
-    @order_item = @order.order_items.find {|item| item.album_id == order_item_params[:album_id].to_i }
-    # if order_item of selected album_id already exists
-    # add requested quantity to that order_item
-    # otherwise, the album is not in the cart
-    # so add it to the cart, with the requested quantity
-    if @order_item
-      @order_item.update(quantity: @order_item.quantity + order_item_params[:quantity].to_i)
-    else
-      @order_item = @order.order_items.create(order_item_params)
-    end
-    if @order_item.save
-      # add dynamic alert message
-      respond_to do |format|
-        format.js
+    if valid_cart_total?(order_item_params)
+      @order_item = find_cart_item_by_id(cart.id, order_item_params[:album_id].to_i)
+      # @order_item =
+      # if order_item of selected album_id already exists
+      # add requested quantity to that order_item
+      # otherwise, the album is not in the cart
+      # so add it to the cart, with the requested quantity
+      if @order_item
+        @order_item.update(quantity: @order_item.quantity + order_item_params[:quantity].to_i)
+      else
+        @order_item = @order.order_items.create(order_item_params)
+      end
+      if @order_item.save
+        # add dynamic alert message
+        respond_to do |format|
+          format.js
+        end
+      else
       end
     else
-      respond_to do |format|
-        format.js
-      end
+      redirect_to albums_path
     end
   end
   private
